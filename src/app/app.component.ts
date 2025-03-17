fb8836dcef79c5e0db4ea8cf4ccb140fa899a2c5
Pang <pangyuyu@illusiontech.cn>
2025-03-17 15:22:18 +0800

    fix：移除不兼容的依赖

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
 import { CommonModule } from '@angular/common';
+import { NgIconsModule } from '@ng-icons/core';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, CommonModule],
+    imports: [RouterOutlet, CommonModule, NgIconsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { NgIconsModule } from '@ng-icons/core';
+// 导入全部图标
+import * as bootstrapIcons from '@ng-icons/bootstrap-icons';
+import * as heroiconsOutline from '@ng-icons/heroicons/outline';
+import * as heroiconsSolid from '@ng-icons/heroicons/solid';
+// 导入路由
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(
+      NgIconsModule.withIcons({
+        ...bootstrapIcons,
+        ...heroiconsOutline,
+        ...heroiconsSolid,
+      }),
+    )
+  ]
 };
