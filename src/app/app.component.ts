1c9e226d926b782f84f6ba8c58a314fd09c77ed0
Pang <pangyuyu@illusiontech.cn>
2025-03-17 15:05:14 +0800

    fix: 调整部分main的代码

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
-    standalone: true,
+    imports: [RouterOutlet, CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
