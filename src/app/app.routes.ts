8dce88dafcaec882e29baabf09816d8f80416041
Pang <pangyuyu@illusiontech.cn>
2025-08-28 10:11:10 +0800

    chore(andorid): 修复路由问题，依赖问题

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,8 @@ import { Routes } from "@angular/router";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'page', pathMatch: 'full' },
+  {
+    path: 'page',
+    loadComponent: () => import('./pages/page.component').then((mod) => mod.PageComponent),
+  }
 ];
